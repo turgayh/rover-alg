c7c3d76162839500602fcd6cd989da6f1cc42af9
hakanturgay <turgayh@itu.edu.tr>
2021-03-06 02:41:39 +0300

    direction case

--- Program.cs
+++ Program.cs
@@ -4,6 +4,14 @@ namespace rover_alg
 {
     class Program
     {
+        public enum Direction
+        {
+            North = 'N',
+            South = 'S',
+            Westh = 'W',
+            Easth = 'E'
+        }
+
         abstract class Vehicle
         {
             public int x {get;set;}
@@ -14,13 +22,38 @@ namespace rover_alg
 
         class Rover:Vehicle
         {
+            public Rover(int x, int y , char direction)
+            {
+                this.x = x;
+                this.y = y;
+                this.direction = direction;
+            } 
             public override void Move()
             {
-
+                switch(this.direction)
+                {
+                    case (char)Direction.North:
+                        this.y += 1;
+                        break;
+                    case (char)Direction.South:
+                        this.y -= 1;
+                        break; 
+                    case (char)Direction.Easth:
+                        this.x += 1;
+                        break;
+                    case (char)Direction.Westh:
+                        this.x -= 1;
+                        break;
+                    default:
+                       break; 
+                }
             }
         }
+
         static void Main(string[] args)
         {
+            Rover q1 = new Rover(1,2,'N');
+            q1.Move();
             Console.WriteLine("Hello World!!");
         }
     }
