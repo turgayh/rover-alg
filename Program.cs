06bde6dbcb4211de89deb45c54c30822b28f06b8
hakanturgay <turgayh@itu.edu.tr>
2021-03-05 23:15:48 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace rover_alg
+{
+    class Program
+    {
+        abstract class Vehicle
+        {
+            public int x {get;set;}
+            public int y {get;set;}
+            public char direction {get;set;}
+            public abstract void Move();
+        }
+
+        class Rover:Vehicle
+        {
+            public override void Move()
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!");
+        }
+    }
+}
